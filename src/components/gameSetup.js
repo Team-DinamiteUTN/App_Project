49ed04080fa11a6aaabd1a0fec74e99e46e8d30b
dferreto <diferretomo@est.utn.ac.cr>
2024-04-27 15:41:03 -0600

    Conexión socket para dos jugadores simultaneos

--- src/components/gameSetup.js
+++ src/components/gameSetup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, Text, TouchableOpacity, Alert } from "react-native";
+import { View, TextInput, Text, TouchableOpacity, Alert, ActivityIndicator } from "react-native";
 import axios from "axios";
 import { ws } from "../../App";
 import { useNavigation } from "@react-navigation/native"; 
@@ -10,9 +10,11 @@ const GameSetup = () => {
   const [playerName, setPlayerName] = useState("");
   const [playerCount, setPlayerCount] = useState(0);
   const [playerRegistered, setPlayerRegistered] = useState(false);
+  const [isReadyToNavigate, setIsReadyToNavigate] = useState(false); // Estado para controlar la navegación
+  const [loading, setLoading] = useState(false); // Estado para controlar la carga
   const navigation = useNavigation(); 
 
-  // Verificar el número de jugadores al montar el componente
+ 
   useEffect(() => {
     ws.onmessage = (e) => {
       const playerNameFromServer = e.data;
@@ -22,6 +24,14 @@ const GameSetup = () => {
     };
   }, []);
 
+ 
+  useEffect(() => {
+    if (playerCount >= 2) {
+      setIsReadyToNavigate(true);
+      setLoading(false); 
+    }
+  }, [playerCount]);
+
   const sendPlayer = async () => {
     if (!playerName.trim()) {
       console.log("El nombre del jugador no puede estar vacío");
@@ -29,41 +39,54 @@ const GameSetup = () => {
       return;
     }
 
-    if (playerCount < 2) {
-      ws.send(playerName);
-      setPlayerName("");
+    setLoading(true); 
+    ws.send(playerName);
+    setPlayerName("");
 
-      try {
-        const data = {
-          Name: playerName,
-          Points: 0,
-        };
-        await axios.post(`${PATHURL}:${PORT}/player`, data);
-        setPlayerName("");
-        navigation.navigate("Tercera"); 
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      console.log("Ya hay 4 jugadores registrados. No se puede unir más jugadores.");
-      Alert.alert("", "Ya hay 4 jugadores registrados. No se puede unir más jugadores.");
+    try {
+      const data = {
+        Name: playerName,
+        Points: 0,
+      };
+      await axios.post(`${PATHURL}:${PORT}/player`, data);
+      setPlayerName("");
+    } catch (error) {
+      console.error(error);
     }
   };
 
+  // Función para navegar a la tercera pantalla
+  useEffect(() => {
+    if (isReadyToNavigate) {
+      navigation.navigate("Tercera"); 
+    }
+  }, [isReadyToNavigate]);
+
   return (
     <View style={style_segunda.whiteBox}>
-      <Text style={style_segunda.title2}>Nombre del jugador</Text>
-      <TextInput
-        style={style_segunda.input}
-        placeholder="Ingresa tu nombre"
-        value={playerName}
-        onChangeText={(newName) => {
-          setPlayerName(newName);
-        }}
-      />
-      <TouchableOpacity style={style_segunda.button} onPress={sendPlayer}>
-        <Text style={style_segunda.buttonText}>Jugar</Text>
-      </TouchableOpacity>
+      {loading && (
+        
+        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text style={{ marginTop: 10 }}>Esperando al otro jugador...</Text>
+        </View>
+      )}
+      {!loading && (
+        <>
+          <Text style={style_segunda.title2}>Nombre del jugador</Text>
+          <TextInput
+            style={style_segunda.input}
+            placeholder="Ingresa tu nombre"
+            value={playerName}
+            onChangeText={(newName) => {
+              setPlayerName(newName);
+            }}
+          />
+          <TouchableOpacity style={style_segunda.button} onPress={sendPlayer}>
+            <Text style={style_segunda.buttonText}>Jugar</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 };
